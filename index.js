fce897ce33aa562e904d41137316e33bbc97f962
Richard Trapp <richardtrapp@leandrodcc.local>
2020-07-30 14:00:29 -0300

    css changes

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const express = require("express");
 const serrver = http.createServer();
 const app = express();
 
+app.use("/styles", express.static("styles"));
+app.use("/assets", express.static("assets"));
 app.get("/", (req,res) => res.sendFile(__dirname + "/index.html"));
+app.post("/tagbatch", (req,res) => console.log("teste"));
 app.listen(3000, () => console.log("Listening on port 3000"));
